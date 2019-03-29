b33de167ab79f30686e987c7e1c0446508e9b926
root <root@instance-1.us-east1-b.c.polar-cyclist-235904.internal>
2019-03-29 09:44:45 +0000

    Revert "try - fp optimization from suspend-resume functionality"
    This reverts commit 3b7b1c83106329f833d9e1373d6753712aaa0b1c.

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -463,15 +463,15 @@ static void fpc1020_suspend_resume(struct work_struct *work)
 		container_of(work, typeof(*fpc1020), pm_work);
 
 	if (fpc1020->screen_state) {
-		set_fingerprintd_nice(-1);
+		set_fpc_irq(fpc1020, true);
+		set_fingerprintd_nice(0);
 	} else {
 		/*
 		 * Elevate fingerprintd priority when screen is off to ensure
 		 * the fingerprint sensor is responsive and that the haptic
 		 * response on successful verification always fires.
 		 */
-		set_fingerprintd_nice(0);
-		set_fpc_irq(fpc1020, true);
+		set_fingerprintd_nice(-1);
 	}
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL,
